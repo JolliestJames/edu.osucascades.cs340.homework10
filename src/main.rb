1fe32c7dc061b46ff0910a1e83448cc9a12ea4c7
ToBeAmped <martinja@oregonstate.edu>
2018-03-13 14:12:28 -0700

    Clean up the damn code

--- src/main.rb
+++ src/main.rb
@@ -6,8 +6,8 @@ Mongo::Logger.logger.level = ::Logger::FATAL
 
 client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'testdb')
 
-client.collections.each { |coll| puts coll.name }
+client.collections.each { |c| puts c.name }
 
-client[:guitars].find.each { |entity| puts entity }
+client[:guitars].find.each { |g| puts g }
 
 client.close
